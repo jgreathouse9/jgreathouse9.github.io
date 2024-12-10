21138747e13749186f8d219b9b5fa81c40d06bbe
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-12-09 21:35:57 -0500

    Update visamain.py

--- Python/Scrapers/Visa/visamain.py
+++ Python/Scrapers/Visa/visamain.py
@@ -1,5 +1,8 @@
 from visautils import load_data, plot_data
+import matplotlib.pyplot as plt
 
 url = "https://usa.visa.com/content/dam/VCOM/regional/na/us/partner-with-us/economic-insights/documents/vbei-visa-us-smi-data-appendix.xlsx"
 
 plot_data(load_data(url))
+
+plt.savefig("Python/Scrapers/Visa/VisaSpending.png")

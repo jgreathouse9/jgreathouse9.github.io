07fa1d9f8f583660f4645192e2efb7ede593afab
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-12-09 21:23:34 -0500

    Create visamain.py

---
+++ Python/Scrapers/Visa/visamain.py
@@ -0,0 +1,5 @@
+
+
+url = "https://usa.visa.com/content/dam/VCOM/regional/na/us/partner-with-us/economic-insights/documents/vbei-visa-us-smi-data-appendix.xlsx"
+
+plot_visa_data(load_and_process_data(url))

5ca0547d276b59704fcb68271166bf49647b230a
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-12-09 21:24:25 -0500

    Update visamain.py

--- Python/Scrapers/Visa/visamain.py
+++ Python/Scrapers/Visa/visamain.py
@@ -1,5 +1,5 @@
-
+from visautils import 
 
 url = "https://usa.visa.com/content/dam/VCOM/regional/na/us/partner-with-us/economic-insights/documents/vbei-visa-us-smi-data-appendix.xlsx"
 
-plot_visa_data(load_and_process_data(url))
+plot_data(load__data(url))

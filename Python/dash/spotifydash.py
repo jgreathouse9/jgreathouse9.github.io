393d338a38165d1f5b1122b0717d41b67c168d85
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-12-18 16:48:24 -0500

    Update spotifydash.py

--- Python/dash/spotifydash.py
+++ Python/dash/spotifydash.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go  # Import Plotly
 st.set_page_config(layout="wide", page_title="Artist Trends")
 
 # Display a general message at the top
-st.markdown("## I'm a music nerd, but especially for (neo)soul. I'm also a fan of the COLORS show on YouTube. Here is some Spotify data I scraped together.")
+st.markdown("## I'm a music nerd, but especially for (neo)soul. I'm also a fan of the [COLORS](https://www.youtube.com/@COLORSxSTUDIOS) show on YouTube. In this spirit, I scraped some of their Spotify data and made an app of it.")
 
 
 
